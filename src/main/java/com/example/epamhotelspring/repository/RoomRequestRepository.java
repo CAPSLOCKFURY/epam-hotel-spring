53bf33588ae8c7af86037560a1dd34258e19853f
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-03-28 16:38:56 +0300

    fixed room request repository not showing room class translation due to wrong left join

--- src/main/java/com/example/epamhotelspring/repository/RoomRequestRepository.java
+++ src/main/java/com/example/epamhotelspring/repository/RoomRequestRepository.java
@@ -12,10 +12,9 @@ import java.util.List;
 public interface RoomRequestRepository extends JpaRepository<RoomRequest, Long> {
 
     @Query("select r.id as id, r.capacity as capacity, rct.name as roomClass, r.checkInDate as checkInDate, " +
-            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, room.id as roomId, r.managerComment as managerComment " +
+            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, r.room.id as roomId, r.managerComment as managerComment " +
             "from RoomRequest r " +
-            "left join r.room room " +
-            "left join room.roomClass rc " +
+            "left join r.roomClass rc " +
             "left join rc.roomClassTranslations rct on rct.language = ?#{T(org.springframework.context.i18n.LocaleContextHolder).getLocale().toLanguageTag()} " +
             "where r.user.id = :userId")
     List<RoomRequestDTO> findRoomRequestsByUserId(Long userId);
--- src/main/java/com/example/epamhotelspring/repository/admin/AdminRoomRequestRepository.java
+++ src/main/java/com/example/epamhotelspring/repository/admin/AdminRoomRequestRepository.java
@@ -14,22 +14,20 @@ import java.util.List;
 public interface AdminRoomRequestRepository extends JpaRepository<RoomRequest, Long> {
 
     @Query("select r.id as id, r.capacity as capacity, rct.name as roomClass, r.checkInDate as checkInDate, " +
-            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, room.id as roomId, r.managerComment as managerComment, " +
+            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, r.room.id as roomId, r.managerComment as managerComment, " +
             "user.firstName as userFirstName, user.lastName as userLastName, user.email as userEmail, user.username as userUsername " +
             "from RoomRequest r " +
-            "left join r.room room " +
-            "left join room.roomClass rc " +
+            "left join r.roomClass rc " +
             "left join rc.roomClassTranslations rct on rct.language = :locale " +
             "left join r.user user"
     )
     List<AdminRoomRequestDTO> findAdminRoomRequests();
 
     @Query("select r.id as id, r.capacity as capacity, rct.name as roomClass, r.checkInDate as checkInDate, " +
-            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, room.id as roomId, r.managerComment as managerComment, " +
+            "r.checkOutDate as checkOutDate, r.comment as comment, r.status as status, r.room.id as roomId, r.managerComment as managerComment, " +
             "user.firstName as userFirstName, user.lastName as userLastName, user.email as userEmail, user.username as userUsername " +
             "from RoomRequest r " +
-            "left join r.room room " +
-            "left join room.roomClass rc " +
+            "left join r.roomClass rc " +
             "left join rc.roomClassTranslations rct on rct.language = ?#{T(org.springframework.context.i18n.LocaleContextHolder).getLocale().toLanguageTag()} " +
             "left join r.user user " +
             "where r.id = :requestId "
