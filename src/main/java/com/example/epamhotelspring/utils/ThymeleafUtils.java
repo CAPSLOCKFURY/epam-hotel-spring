2f968281df3e12f1bab1561134d1b6e888fc6bde
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-04-01 22:17:59 +0300

    changed buildGetParameters signature to String - List String type

--- src/main/java/com/example/epamhotelspring/utils/ThymeleafUtils.java
+++ src/main/java/com/example/epamhotelspring/utils/ThymeleafUtils.java
@@ -1,28 +1,26 @@
 package com.example.epamhotelspring.utils;
 
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service("thymeleafUtils")
 public class ThymeleafUtils {
 
     /**
-     * Joins given map of get params with ampresand
+     * Joins given map of get params with ampersand
      * @param params Map of get parameters
      * @param ignoreParams Parameter names that should be ignored from joining
      * @return Joined string of get params, delimiter is ampersand
      */
-    public String buildGetParameters(Map<String, String> params, String ...ignoreParams){
+    public String buildGetParameters(Map<String, List<String>> params, String ...ignoreParams){
         if(params == null || params.isEmpty()) {
             return "";
         }
         params.keySet().removeAll(Set.of(ignoreParams));
         return params.entrySet().stream()
+                .flatMap(e -> e.getValue().stream().map(value -> new AbstractMap.SimpleEntry<>(e.getKey(), value)))
                 .map(e -> e.getKey().concat("="+e.getValue()))
                 .collect(Collectors.joining("&"));
     }
