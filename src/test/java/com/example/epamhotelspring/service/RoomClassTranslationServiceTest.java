f822e97fd63bfbf6ec55ce1fbdf146a3f241f504
CAPSLOCKFURY <vadim.dembitskii@gmail.com>
2022-04-11 20:07:22 +0300

    Added RoomClassTranslationServiceTest

---
+++ src/test/java/com/example/epamhotelspring/service/RoomClassTranslationServiceTest.java
@@ -0,0 +1,49 @@
+package com.example.epamhotelspring.service;
+
+import com.example.epamhotelspring.dto.RoomClassTranslationDTO;
+import com.example.epamhotelspring.model.RoomClass;
+import com.example.epamhotelspring.model.RoomClassTranslation;
+import com.example.epamhotelspring.repository.RoomClassRepository;
+import com.example.epamhotelspring.repository.RoomClassTranslationRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@Transactional
+@TestPropertySource(locations = "/application-test.properties")
+public class RoomClassTranslationServiceTest {
+
+    @Autowired
+    private RoomClassTranslationService roomClassTranslationService;
+
+    @Autowired
+    private RoomClassTranslationRepository roomClassTranslationRepository;
+
+    @Autowired
+    private RoomClassRepository roomClassRepository;
+
+    @Test
+    void getRoomClassesByLanguageTest(){
+        RoomClass roomClass = new RoomClass();
+        roomClass = roomClassRepository.save(roomClass);
+        RoomClassTranslation ruRoomClassTranslation = new RoomClassTranslation().setRoomClass(roomClass).setLanguage("ru").setName("ruName");
+        roomClassTranslationRepository.save(ruRoomClassTranslation);
+        RoomClassTranslation enRoomClassTranslation = new RoomClassTranslation().setRoomClass(roomClass).setLanguage("en").setName("enName");
+        roomClassTranslationRepository.save(enRoomClassTranslation);
+        List<RoomClassTranslationDTO> roomClassTranslations = roomClassTranslationRepository.findRoomClassTranslationsByLanguage("en");
+        List<String> roomClassTranslationNames = roomClassTranslations.stream().map(RoomClassTranslationDTO::getName).collect(Collectors.toList());
+        assertTrue(roomClassTranslationNames.contains("enName") && !roomClassTranslationNames.contains("ruName"));
+    }
+
+
+}
