e906753ffd202528b516d7bbdc7b985e59924f43
CAPSLOCKFURY <vadim.dembitskii@gmail.com>
2022-05-24 19:16:07 +0300

    Made one more test for room closing and refunds

--- src/test/java/com/example/epamhotelspring/service/AdminRoomServiceTest.java
+++ src/test/java/com/example/epamhotelspring/service/AdminRoomServiceTest.java
@@ -86,4 +86,38 @@ public class AdminRoomServiceTest {
         assertFalse(billingRepository.existsById(user2Billing.getId()));
     }
 
+    @Test
+    void closeRoomSummingRefundTest(){
+        User user1 = new User("user1", "pass", "email1@gmail.com", "User1", "User1");
+        User user2 = new User("user2", "pass", "email2@gmail.com", "User2", "User2").setBalance(new BigDecimal(100));
+        RoomClass roomClass = new RoomClass();
+        roomClassRepository.save(roomClass);
+        Room room = new Room(1, "1", 4, new BigDecimal(100), roomClass);
+        userRepository.save(user1);
+        userRepository.save(user2);
+        roomRepository.save(room);
+        LocalDate today = LocalDate.now();
+        LocalDate weekAway = today.plus(7, ChronoUnit.DAYS);
+        LocalDate twoWeeksAway = weekAway.plus(7, ChronoUnit.DAYS);
+        LocalDate threeWeeksAway = twoWeeksAway.plus(7, ChronoUnit.DAYS);
+
+        RoomRegistry user1RoomRegistry1 = new RoomRegistry(room, today, weekAway,user1);
+        RoomRegistry user1RoomRegistry2 = new RoomRegistry(room, weekAway, twoWeeksAway,user1);
+        RoomRegistry user1RoomRegistry3 = new RoomRegistry(room, twoWeeksAway.plus(3, ChronoUnit.DAYS), twoWeeksAway.plus(7, ChronoUnit.DAYS),user1);
+        RoomRegistry user2RoomRegistry1 = new RoomRegistry(room, threeWeeksAway.plus(8, ChronoUnit.DAYS), threeWeeksAway.plus(10, ChronoUnit.DAYS),user2);
+        RoomRegistry user2RoomRegistry2 = new RoomRegistry(room, threeWeeksAway.plus(11, ChronoUnit.DAYS), threeWeeksAway.plus(14, ChronoUnit.DAYS),user2);
+        roomRegistryRepository.save(user1RoomRegistry1);
+        roomRegistryRepository.save(user1RoomRegistry2);
+        roomRegistryRepository.save(user1RoomRegistry3);
+        roomRegistryRepository.save(user2RoomRegistry1);
+        roomRegistryRepository.save(user2RoomRegistry2);
+
+        adminRoomService.closeRoom(new CloseRoomForm(today, threeWeeksAway.plus(16, ChronoUnit.DAYS), room.getId()));
+
+        User dbUser1 = userRepository.getById(user1.getId());
+        User dbUser2 = userRepository.getById(user2.getId());
+        assertEquals(new BigDecimal("600.00"), dbUser2.getBalance());
+        assertEquals(new BigDecimal("1800.00"), dbUser1.getBalance());
+    }
+
 }
